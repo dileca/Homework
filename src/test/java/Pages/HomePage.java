2d092d8406309c188d2122d1c7b2174cccb45cde
DianaL <dianaleca1@gmail.com>
2025-10-17 18:10:13 +0200

    created the home page class and created the verifyHomePageIsDisplayed method

--- src/test/java/Pages/HomePage.java
+++ src/test/java/Pages/HomePage.java
@@ -1,11 +1,23 @@
 package Pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class HomePage {
 
     WebDriver driver;
 
-    @FindBy(id = )
+    @FindBy(id = "overview-hero")
+    WebElement homepageTitle_id;
+
+    public HomePage(WebDriver driver){
+        this.driver=driver;
+    }
+
+    public void verifyHomePageIsDisplayed(){
+        homepageTitle_id.isDisplayed();
+    }
+
+
 }
